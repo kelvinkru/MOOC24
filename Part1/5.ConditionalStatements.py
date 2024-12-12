84705191f4387c65e79dffb3e886a63ae2203385
kelvinkru <kelvinkrug@gmail.com>
2024-12-12 16:10:51 -0500

    init

--- Part1/5.ConditionalStatements.py
+++ Part1/5.ConditionalStatements.py
@@ -6,9 +6,15 @@ def absval():
         abs = nr
     print(f"Abs value {abs}")
 
-def magn:
+def magn():
     nr = int(input("Enter a number: "))
-
+    if nr < 1000:
+        print("Smaller than 1,000")
+    if nr < 100:
+        print("Smaller than 100")
+    if nr < 10:
+        print("Smaller than 10")
+    print("Thanks")
 
 if __name__ == '__main__':
-    absval()
+    magn()
