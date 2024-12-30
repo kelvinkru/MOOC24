d919193084953d734ef9a47b0dc53eb12f5983bf
kelvinkru <kelvinkrug@gmail.com>
2024-12-30 13:55:53 -0500

    init

--- Part3/1.LoopsWithCond.py
+++ Part3/1.LoopsWithCond.py
@@ -10,5 +10,27 @@ def cntdwn():
         nr-=1
     print('Now!')
 
+def nr1():
+    number = int(input("Enter a number: "))
+    i = 1
+    while i <= number-1:
+        print(i)
+        i+=1
+
+def powr_two():
+    limit = int(input("Enter Upper Limit: "))
+    i = 1
+    while i<=limit:
+        print(i)
+        i*=2
+
+def base():
+    limit = int(input("Enter Upper Limit: "))
+    base = int(input("Enter Base Number: "))
+    i = 1
+    while i<=limit:
+        print(i)
+        i*=base
+
 if __name__ == '__main__':
-    cntdwn()
+    base()
