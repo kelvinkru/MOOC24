afe89132cb9bd490bb6a7237caff452dbefc89f4
kelvinkru <kelvinkrug@gmail.com>
2025-01-02 13:16:23 -0500

    init

--- Part3/2.Strings.py
+++ Part3/2.Strings.py
@@ -69,15 +69,40 @@ def fisrt_subs():
 def all_subs():
     word = str(input('Type in a word: '))
     char = str(input('Type in a character: '))
-    for l in range(len(word)):
-        if l==0:
-            word2 = word[l:]
-        else:
-            word2 = word[l+inx+1:]
+    n = 0
+    while True:
+        word2 = word[n:]
         inx = word2.find(char)
         if inx != -1:
             if len(word2) - inx>3:
                 print(word2[inx:inx+3])
+                n+=inx+len(char)
+            else:
+                break
+        else:
+            break
+
+def scnd_occ():
+    word = str(input('Type in a word: '))
+    char = str(input('Type in a character: '))
+    n = 0
+    occ = 0
+    while True:
+        word2 = word[n:]
+        inx = word2.find(char)
+        if inx != -1:
+            occ+=1
+            if len(word2) >0:
+                if occ==2:
+                    print(f'second occurrence  at {n + inx}')
+                    break
+                n+=inx+len(char)
+            else:
+                break
+        else:
+            break
+    if occ<2:
+        print('does not appear twice')
 
 if __name__ == '__main__':
-    all_subs()
+    scnd_occ()
